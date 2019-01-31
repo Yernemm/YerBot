b06761d227b6109a13fe7514cb8ad8da677dedb7
Yernemm <yernemm@yernemm.xyz>
2019-01-31 23:04:19 +0000

    Create pettyspoiler.js

---
+++ commands/pettyspoiler.js
@@ -0,0 +1,41 @@
+//METADATA
+const desc = "Converts the message such that each character is surrounded by a spoier tag."; //Short description of what the command does.
+const usage = "<message>"; //Any parameters required for command.
+const cmdtype = "fun"; //Type of command
+//Command
+exports.run = (config, client, message, argsArr, argsTxt, extraData) => {
+    const bot = client;
+    const m = require("./../shared/methods.js");
+    //--------------------------------------------------------------------
+    //Uncomment for protected owner-only command.
+    //if(message.author.id !== config.ownerID) return; 
+    //--------------------------------------------------------------------
+    var msg = "`"; //Put response message to be logged here.
+    //--------------------------------------------------------------------
+
+    //COMMAND LOGIC HERE:
+
+    var i = 1;
+
+    message.forEach(e=>{
+        if(i<=398)
+        msg+="||"+e+"||";
+        i++;
+    })
+    msg +="`"
+
+
+
+    //--------------------------------------------------------------------
+    m.logSend(config, client, message, msg); //Method will send msg to user, and also log it in both console AND log channel.
+    //m.log(config, client, message, msg); //Alternative will log msg without sending msg.
+}
+exports.desc = () =>{
+    return desc;
+}
+exports.use = () =>{
+    return usage;
+}
+exports.cmdtype = () => {
+    return cmdtype;
+}
